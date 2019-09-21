eabb771b9c5e6381a55342f58d8bb02dfa75f57b
Denis <38587958+Thegrep01@users.noreply.github.com>
2019-09-22 00:13:21 +0300

    Update mian.py

--- mian.py
+++ mian.py
@@ -23,7 +23,7 @@ def print_tree(root):
     print_tree(root.right)
 
 
-def get_token(token_list, expected):
+def get_token(token_list, expected): # проверка на валидность оператора
     if token_list[0] == expected and expected in tokens:
         del token_list[0]
         return True
@@ -32,35 +32,35 @@ def get_token(token_list, expected):
 
 
 def get_symbol(token_list):
-    if get_token(token_list, '('):
+    if get_token(token_list, '('): # избавляемся от скобок если есть
         x = get_line(token_list)  # get the subexpression
         get_token(token_list, ')')  # remove the closing parenthesis
         return x
-    else:
+    else: # создаем лист с символом
         x = token_list[0]
-        if x not in G:
+        if x not in G: # проверка на валидность символа
             raise Exception('Wrong Expression: symbol ' + x + ' is not in G')
         token_list[0:1] = []
         return Tree(x, None, None)
 
 
 def get_dot(token_list):
-    a = get_symbol(token_list)
-    if get_token(token_list, '.'):
-        b = get_dot(token_list)  # this line changed
+    a = get_symbol(token_list) # берем символ 
+    if get_token(token_list, '.'): # если операция . идем дальше и создаем нод дерева
+        b = get_dot(token_list)
         return Tree('.', a, b)
-    elif token_list[0] not in tokens and token_list[0] not in G:
+    elif token_list[0] not in tokens and token_list[0] not in G: # проверка на то входит ли символ в алфовит или набор операторов
         raise Exception('Wrong Expression: symbol ' + token_list[0])
-    else:
+    else: # если операция не . просто возвращаем символ
         return a
 
 
 def get_line(token_list):
-    a = get_dot(token_list)
-    if get_token(token_list, '|'):
+    a = get_dot(token_list) # берем операцию с .
+    if get_token(token_list, '|'): # если операция | идем дальше и создаем нод дерева
         b = get_line(token_list)
         return Tree('|', a, b)
-    else:
+    else: # если операция не | возращаем результат работы get_dot
         return a
 
 
@@ -68,5 +68,5 @@ def get_line(token_list):
 # print_tree(tree)
 
 token_list = prep_string('a | ( ( b . c ) . a )')
-tree = get_line(token_list)
+tree = get_line(token_list) # предпологаем что первая операция |
 print_tree(tree)
